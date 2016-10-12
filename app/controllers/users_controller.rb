e0e79e9816c83595540a5ae9250f54db075bbc7d
Marcelo Soares Souza <marcelo@riseup.net>
2016-10-12 16:07:22 -0300

    * Added Locale pt-BR, defined pt-BR has default locale * Added Gravatar Reference for users. * Minor Adjustments in Layout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_if_admin
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # Users Routes
   devise_for :users
+  resources :users
+
+  # Main Page Route
   get 'home/index'
   root to: "home#index"
 end
