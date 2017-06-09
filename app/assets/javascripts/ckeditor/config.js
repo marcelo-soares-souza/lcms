d9c6d48a03cdff9503b4a075cceabb42470c6d10
Marcelo Soares Souza <marcelo@itbio3.org>
2017-06-09 12:01:03 -0300

    Minor fixes in ckEditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -4,6 +4,8 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+        config.basePath = '/assets/ckeditor/';
+        config.pluginDirectory = '/assets/ckeditor/';
         config.skin = 'moono,/assets/ckeditor/';
 
 	// Define changes to default configuration here.
@@ -11,6 +13,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
+        /*
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
@@ -27,7 +30,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors' },
 		{ name: 'about' }
 	];
-
+        */
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	// config.removeButtons = 'Underline,Subscript,Superscript';
